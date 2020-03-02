0a16b3f3826b4dcd9476f9327f234707cc5564da
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-02 14:47:36 -0800

    update shell script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-touch ../MERN-Client-Backend/.env
-echo 'PORT=4000\nMONGO_URI=mongodb://localhost/wayfarer\nJWT_SECRET=' > ../MERN-Client-Backend/.env
-echo '.env' >> ../MERN-Client-Backend/.gitignore
-
+touch ../MERN-Client-Backend/.env2
+echo 'PORT=4000' >> ../MERN-Client-Backend/.env2
+echo 'MONGO_URI=mongodb://localhost/wayfarer' >>../MERN-Client-Backend/.env2
+echo 'JWT_SECRET=' >> ../MERN-Client-Backend/.env2
+echo '.env2' >> ../MERN-Client-Backend/.gitignore
 node seed.js
