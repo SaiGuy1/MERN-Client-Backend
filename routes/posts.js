52898b1450b8e3207314583cc75316028e96e98a
jonathan-crepeau <jon.crepeau@gmail.com>
2020-03-03 14:19:42 -0800

    create and test show all posts (all users)

--- routes/posts.js
+++ routes/posts.js
@@ -5,13 +5,16 @@ const ctrl = require('../controllers');
 
 // PATH = '/api/v1/posts'
 
+// Controller To Show All Posts (Regardless Of User)
+router.get('/show', ctrl.posts.showAll);
+
 // Controller To Create Single Post
 router.post('/create', mw.auth.verify, ctrl.posts.create);
 
 // Controller To Show Single Post
 router.get('/:id', mw.auth.verify, ctrl.posts.show);
 
-// Controller To Show All User's Posts
-router.get('/', mw.auth.verify, ctrl.posts.showAll);
+// Controller For Single User To Show All Of Their Own Posts
+router.get('/userall', mw.auth.verify, ctrl.posts.userAllPosts);
 
 module.exports = router;
