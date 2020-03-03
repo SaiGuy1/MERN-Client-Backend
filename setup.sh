87e1f681caa525e2552099da2e40e89b9033bb53
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-03 00:35:49 -0800

    update gitignore and setup file

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ touch ../MERN-Client-Backend/.env
 echo '' > ../MERN-Client-Backend/.env
 echo 'PORT=4000' >> ../MERN-Client-Backend/.env
 echo 'MONGO_URI=mongodb://localhost/wayfarer' >>../MERN-Client-Backend/.env
-echo 'JWT_SECRET=' >> ../MERN-Client-Backend/.env
+echo 'JWT_SECRET=THIS_IS_SUPER_SECRET' >> ../MERN-Client-Backend/.env
 echo '.env' >> ../MERN-Client-Backend/.gitignore
 node seed.js
