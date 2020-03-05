dea5ab5663f081056acadbe3f15b292ba63f8877
jonathan-crepeau <jon.crepeau@gmail.com>
2020-03-05 09:29:06 -0800

    destroy Post route test successful

--- controllers/posts.js
+++ controllers/posts.js
@@ -19,7 +19,7 @@ const create = async (req, res) => {
 
 const show = async (req, res) => {
     try {
-        const foundPost = await db.Post.findById(req.params.id)
+        const foundPost = await db.Post.findById(req.params.id);
         const Post = {
             id: foundPost._id,
             title: foundPost.title,
@@ -34,7 +34,7 @@ const show = async (req, res) => {
 
 const userAllPosts = async (req, res) => {
     try {
-        const allPosts = await db.Post.find({ user: req.curUserId })
+        const allPosts = await db.Post.find({ user: req.curUserId });
         let postList = [];
         for (let i = 0; i < allPosts.length; i++) {
             let responseObj = {
@@ -42,8 +42,8 @@ const userAllPosts = async (req, res) => {
                 title: allPosts[i].title,
                 content: allPosts[i].content,
                 createdAt: allPosts[i].createdAt,
-            }
-            postList.push(responseObj)
+            };
+            postList.push(responseObj);
         };
         res.status(200).json(postList);
     } catch (error) {
@@ -53,32 +53,45 @@ const userAllPosts = async (req, res) => {
 
 const showAll = async (req, res) => {
     try {
-        const allPosts = await db.Post.find(req.query)
+        const allPosts = await db.Post.find(req.query);
         res.status(200).json(allPosts);
     } catch (error) {
         return res.status(500).json({ message: 'Something went wrong, try again', error: error });
-    }
-}
+    };
+};
 
-// const update = async (req, res) => {
-//     try {
-//         const updatedPost = await db.Post.findOneAndUpdate(...req.body)
-//         const responseObj = {
-//             id: updatedPost._id,
-//             title: updatedPost.title,
-//             content: updatedPost.content,
-//             location: updatedPost.location,
-//             createdAt: updatedPost.createdAt,
-//         }
-//         res.status(200).json(responseObj);
-//     } catch (error) {
-//         return res.status(500).json({message: 'Something went wrong, please try again', error: error})
-//     }
-// }
+const update = async (req, res) => {
+    try {
+        if (req.curUserId = Post.user) {
+            const updatedPost = await db.Post.findByIdAndUpdate(req.params.id, req.body);
+            const responseObj = {
+                id: updatedPost.id,
+                title: updatedPost.title,
+                content: updatedPost.content,
+                location: updatedPost.location,
+                createdAt: updatedPost.createdAt
+            };
+            res.status(200).json(responseObj);
+        };
+    } catch (error) {
+        return res.status(500).json({message: 'Something went wrong, please try again', error: error});
+    };
+};
+
+const destroy = async (req, res) => {
+    try {
+        const deletedPost = await db.Post.findByIdAndDelete(req.params.id);
+        res.status(200).json(deletedPost);
+    } catch (error) {
+        res.status(500).json({message: 'Something went wrong, please try again', error: error});
+    };
+};
 
 module.exports = {
     create,
     show,
     userAllPosts,
     showAll,
+    update,
+    destroy
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,8 @@ const ctrl = require('../controllers');
 
 // PATH = '/api/v1/posts'
 
-// Controller To Show All Posts (Regardless Of User)
+// Controller To Show All Posts
+// See README.
 router.get('/', ctrl.posts.showAll);
 
 // Controller For Single User To Show All Of Their Own Posts
@@ -17,6 +18,10 @@ router.post('/create', mw.auth.verify, ctrl.posts.create);
 // Controller To Show Single Post
 router.get('/:id', mw.auth.verify, ctrl.posts.show);
 
+// Controller To Update Single Post
+router.put('/:id', mw.auth.verify, ctrl.posts.update);
 
+// Controller To Delete Single Post
+router.delete('/:id', mw.auth.verify, ctrl.posts.destroy);
 
 module.exports = router;
